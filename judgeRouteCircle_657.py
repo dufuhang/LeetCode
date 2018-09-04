7458dfb082d02ea5b92817faab4b38e157e60278
杜府航 <dufuhang@dufuhang.local>
2018-09-04 18:54:51 +0800

    solution

---
+++ judgeRouteCircle_657.py
@@ -0,0 +1,13 @@
+'''
+只要判断字符中相反的路径，比如R和L，U和D的出现次数是否相等即可
+'''
+class Solution:
+    def judgeCircle(self, moves):
+        numR = moves.count('R')
+        numL = moves.count('L')
+        numU = moves.count('U')
+        numD = moves.count('D')
+        if numR == numL and numU == numD:
+            return True
+        else:
+            return False
