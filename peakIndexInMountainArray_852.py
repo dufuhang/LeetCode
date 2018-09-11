79d155aa6a133aa83a8b19d3774619d75aa03aae
杜府航 <dufuhang@dufuhang.local>
2018-09-11 10:30:52 +0800

    solution

---
+++ peakIndexInMountainArray_852.py
@@ -0,0 +1,18 @@
+'''
+即找出数组中的最大值然后返回下标
+'''
+class Solution:
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        temp = A[0]
+        for value in A:
+            if value > temp:
+                temp = value
+        return A.index(temp)
+        '''
+        直接使用自带的函数：
+        return A.index(max(A))
+        '''
