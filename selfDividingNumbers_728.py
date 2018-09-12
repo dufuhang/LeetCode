c53e9c5e94e6515f5e021ea3b905e4219ebf7191
杜府航 <dufuhang@dufuhang.local>
2018-09-12 11:04:52 +0800

    add explanation

--- selfDividingNumbers_728.py
+++ selfDividingNumbers_728.py
@@ -1,3 +1,7 @@
+'''
+filter()函数用于筛选列表，接收一个函数和一个迭代对象作为参数，将迭代对象的每个值传入函数做判断，然后返回新的迭代对象
+在Python2.x中，filter()返回列表，3.x为返回一个迭代对象！！
+'''
 class Solution:
     def is_selfDividingNumbers(self,num):
         for i in str(num):
