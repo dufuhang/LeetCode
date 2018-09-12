8806a9bd9d311126ed4e66f38c20def88401d93c
杜府航 <dufuhang@dufuhang.local>
2018-09-12 10:23:12 +0800

    update solution

--- selfDividingNumbers_728.py
+++ selfDividingNumbers_728.py
@@ -1,20 +1,13 @@
 class Solution:
+    def is_selfDividingNumbers(self,num):
+        for i in str(num):
+            if i == '0' or num % int(i) !=0:
+                return False
+        return True
     def selfDividingNumbers(self, left, right):
         """
         :type left: int
         :type right: int
         :rtype: List[int]
         """
-        result=[]
-        for num in range(left,right+1):
-            temp=[]
-            for i in str(num):
-                if num % int(i) == 0 and i != '0':
-                    temp.append(i)
-                else:
-                    break
-            result.append(''.join(temp)
-        print(result) 
-
-s = Solution()
-s.selfDividingNumbers(1,22)
+        return list(filter(self.is_selfDividingNumbers,range(left,right+1)))
