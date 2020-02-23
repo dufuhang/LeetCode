397ab94e0038e17552319bb9971b9ef1db175dbb
dufuhang <286046056@qq.com>
2020-02-23 18:27:13 +0800

    DFS方法待补充

---
+++ Array/78-Subsets.cpp
@@ -0,0 +1,22 @@
+//迭代法
+class Solution 
+{
+public:
+    vector<vector<int>> subsets(vector<int>& nums)
+    {
+        vector<vector<int>> vec = {{}};
+        for (int num : nums)
+        {
+            int n = vec.size();
+            for ( int i = 0; i < n; i++)
+            {
+                vector<int> temp;
+                temp = vec[i];
+                temp.push_back(num);
+                vec.push_back(temp);
+            }
+        }
+        return vec;
+    }
+};
+//DFS待补充
